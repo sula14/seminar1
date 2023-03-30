88314d3346b674fca56e551b2b9ae66381ef51ad
Tolege Sultan <sultantulegenov91@gmail.com>
2023-03-30 14:40:53 +0300

    добавил описание задания 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,3 +4,21 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+System.Console.WriteLine("Введите числа >");
+string value;
+value = Console.ReadLine();
+int a = Convert.ToInt32(value);
+string value2;
+value2 = Console.ReadLine();
+int b = Convert.ToInt32(value2);
+string value3;
+value3 = Console.ReadLine();
+int с = Convert.ToInt32(value3);
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+System.Console.Write("max = ");
+System.Console.Write(max);
+
