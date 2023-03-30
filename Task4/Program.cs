9a13ba950ddb137f64bf08679fbee4aa4686e509
Tolege Sultan <sultantulegenov91@gmail.com>
2023-03-31 00:29:54 +0300

    Написал код по заданию 3

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -3,3 +3,17 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+System.Console.WriteLine("Введите число >");
+string value;
+value = Console.ReadLine();
+int N = Convert.ToInt32(value);
+int num = 1;
+
+while (num < N)
+{
+    if (num % 2 == 0)
+    {
+        System.Console.WriteLine(num);
+    }
+    num++;
+}
